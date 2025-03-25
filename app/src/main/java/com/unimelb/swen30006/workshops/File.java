40e01dc7e6bab2f508555367d96880be49816ae1
adidey <adidey27@gmail.com>
2025-03-25 17:09:18 +1100

    changes made

--- app/src/main/java/com/unimelb/swen30006/workshops/File.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/File.java
@@ -2,12 +2,22 @@ package com.unimelb.swen30006.workshops;
 
 // Sample private file class to be replaced by your implementaiton
 class File {
-    public String fileType(){
-        double num = Math.random();
-        if(num<0.5){
-            return "pdf";
-        } else {
-            return "docx";
-        }
+    Date createdDate;
+    String fileData;
+    String fileName;
+
+    public String content() {
+        // Implementation for retrieving file content
+        return fileData;
+    }
+
+    public int fileSize() {
+        // Implementation for retrieving file size
+        return fileData.length();
+    }
+
+    public String fileType() {
+        // Implementation for retrieving file type
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 }
--- app/src/main/java/com/unimelb/swen30006/workshops/Submission.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Submission.java
@@ -2,11 +2,12 @@ package com.unimelb.swen30006.workshops;
 
 // Sample private submission class to be replaced by your implementation
 class Submission {
-    // Return two fake files
-    public File[] includedFiles(){
-        File[] files = new File[2];
-        files[0] = new File();
-        files[1] = new File();
+    int attemptNum;
+    List<File> files;
+    int numFiles;
+
+    public List<File> includedFiles() {
+        // Implementation for retrieving included files
         return files;
     }
 }
